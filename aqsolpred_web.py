6ce8641e99aaf1f1a7df08c2fd137d7d483551d0
Amardeep Singh <amardeeps5201@gmail.com>
2024-11-28 11:37:46 -0500

    Update aqsolpred_web.py

--- aqsolpred_web.py
+++ aqsolpred_web.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 18 14:54:37 2020
 
-@author: Murat Cihan Sorkun
-"""
 
 
 ######################
@@ -146,26 +142,7 @@ generated_descriptors # Skips the dummy first item
 
 
 st.write("""
-# About AqSolPred
-
-AqSolPred is an highly accurate solubility prediction model that consists consensus of 3 ML algorithms (Neural Nets, Random Forest, and XGBoost). AqSolPred is developed using a quality-oriented data selection method described in [1] and trained on AqSolDB [2] largest publicly available aqueous solubility dataset.
-
-AqSolPred showed a top-performance (0.348 LogS Mean Absolute Error) on Huuskonen benchmark dataset [3].
-
-**version:** 1.0s (lite version of v1.0 described in the paper with reduced RFs(n_estimators=200,max_depth=10) but the same performance)
-
-If you are using the predictions from AqSolPred on your work, please cite these papers: [1, 2]
-
-[1] Sorkun, M. C., Koelman, J.M.V.A. & Er, S.  (2020). Pushing the limits of solubility prediction via quality-oriented data selection, Research Square, DOI:https://doi.org/10.21203/rs.3.rs-84771/v1.
-
-[2] Sorkun, M. C., Khetan, A., & Er, S. (2019).  [AqSolDB, a curated reference set of aqueous solubility and 2D descriptors for a diverse set of compounds](https://www.nature.com/articles/s41597-019-0151-1). Scientific data, 6(1), 1-8.
-
-[3] Huuskonen, J. Estimation of aqueous solubility for a diverse set of organic compounds based on molecular topology. Journal of Chemical Informationand Computer Sciences 40, 773–777 (2000).
-
-Special thanks: This web app is developed based on the tutorials and the template of [DataProfessor's repository](https://github.com/dataprofessor/code/tree/master/streamlit/part7). 
-
-                                                                                         
-**Contact:** [Murat Cihan Sorkun](https://www.linkedin.com/in/murat-cihan-sorkun/)
+# Solubility Predictor App
 
 """)
 
